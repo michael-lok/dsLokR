99429fe14116653511973612b907ac0d8c2cbf83
Michael Lok <michaellok4@Michael-Macbook-Pro.local>
2020-11-25 09:26:40 -0500

    eval.roc

--- R/eval_metrics.R
+++ R/eval_metrics.R
@@ -29,3 +29,55 @@ eval.auc <- function(preds, actual) {
 
   (sum(r[i]) - n_pos * (n_pos + 1)/2)/(n_pos * n_neg)
 }
+
+#' Generate Receiver Operating Characteristic Object
+#' @details object is plottable
+#' @param preds vector of predicted probabilities for response (positive class)
+#' @param actual binary vector of actual response
+#' @param plot whether to return ROC curve plot; defaults to `TRUE``
+#' @import ggplot2
+#' @return
+#'
+#' when plot is set to `TRUE`, a ggplot object that visualizes ROC curve
+#' \itemize{
+#'  \item \code{cutoff} will be displayed in color as gradient on the curb
+#'  \item \code{tpr} will be placed on the y-axis representing true positive rate
+#'  \item \code{fpr} will be placed on the x-axis representing false positive rate
+#' }
+#'
+#' when plot is set to `FALSE`, a data.frame object
+#' \itemize{
+#'  \item \code{cutoff} different cutoffs utilized to evaluate performance
+#'     (determined by)
+#'  \item \code{tpr} true positive rate \eqn{TP/(TP + FN)}
+#'  \item \code{fpr} false positive rate \eqn{FP/(TN + FP)}
+#' }
+#' @export
+eval.roc <- function(preds, actual, plot = TRUE) {
+  assertthat::assert_that(is.prob(preds), # helper function in utils.R
+                          msg = "preds must be numeric vector with values between 0 and 1!")
+  assertthat::assert_that(is.binary(actual), # helper function in utils.R
+                          msg = "actual must be binary vector!")
+  assertthat::assert_that(length(preds) == length(actual),
+                          msg = "preds and actual not of the same length!")
+
+  size <- length(actual)
+  n_pos <- sum(actual == 1)
+  n_neg <- length(actual) - n_pos
+  roc <- lapply(c(size:0) / size, FUN = function(x) {
+    pred.class <- as.integer(preds >= x)
+    data.frame(cutoff = x,
+               tpr = sum(pred.class == actual & actual == 1) / n_pos,
+               fpr = sum(pred.class != actual & pred.class == 1) / n_neg)
+  })
+  roc <- do.call("rbind", roc)
+
+  if (plot) {
+    roc_ggplot <- ggplot(roc, aes(x = fpr, y = tpr, color = cutoff)) +
+      geom_step(size = 2)
+    return(roc_ggplot)
+  } else {
+    return(roc)
+  }
+}
+utils::globalVariables(c("cutoff", "fpr", "tpr"))
--- tests/testthat/test-eval-metrics.R
+++ tests/testthat/test-eval-metrics.R
@@ -25,3 +25,16 @@ actual <- sample(c(3:5), size = 10, replace = TRUE)
 
 test_that("eval.auc ensures actual is binary vector",
           expect_error(eval.auc(preds, actual)))
+
+preds <- runif(10)
+actual <- sample(c(0, 1), size = 10, replace = TRUE)
+
+test_that("eval.roc default output is gg/ggplot class",
+          expect_s3_class(eval.roc(preds, actual),
+                          c("gg", "ggplot"),
+                          exact = TRUE))
+
+test_that("eval.roc optional output is data.frame class",
+          expect_s3_class(eval.roc(preds, actual, plot = FALSE),
+                          c("data.frame"),
+                          exact = TRUE))
